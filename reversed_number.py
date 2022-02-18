8febb09eab883d554128aa4c9756c6cc09fcce17
MikhailNovv <99559253+MikhailNovv@users.noreply.github.com>
2022-02-18 19:09:55 +0000

    initial commit

---
+++ reversed_number.py
@@ -0,0 +1,8 @@
+def turn_over(user_number):
+    inverted_number = ''
+    for i in user_number:
+        inverted_number = i + inverted_number
+    return inverted_number
+
+user_number = input('Введите число: ')
+print('inverted_number =', int(turn_over(user_number)))
